a2086614078fa87ae91dd83f71fb119807bbbb24
Nicholas Walton <cactar65@gmail.com>
2019-10-16 13:47:57 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,7 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
   
-  hash = {"rock" => "small_stone", "Boulder" => "large_stone" }
+  hash = {"rock" => "small_stone", "boulder" => "large_stone" }
 
   
 end
